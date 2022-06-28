4939ea28a57b9f87d934aa84a1e8ccc30b29bc57
Jhonnatan Mendez <jhonnatandavid34mc@gmail.com>
2022-06-27 19:04:46 -0500

    fix TopHead

--- components/TopHead.js
+++ components/TopHead.js
@@ -3,7 +3,7 @@ import Head from 'next/head'
 const TopHead = () => {
     return (
         <Head>
-            <meta name="viewport" content="initial-scale=1, width=device-width">
+            <meta name="viewport" content="initial-scale=1, width=device-width"/>
             <tittle>Find your Animal</tittle>
         </Head>
     )
