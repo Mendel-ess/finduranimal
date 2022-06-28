4689c341d162b0aeb87f71750820f5b9607e29e8
Jhonnatan Mendez <jhonnatandavid34mc@gmail.com>
2022-06-27 19:23:22 -0500

    adding some files

--- components/TopHead.js
+++ components/TopHead.js
@@ -1,11 +1,11 @@
 import Head from 'next/head'
 
-const TopHead = () => {
+const TopHead = ({ children }) => {
     return (
         <Head>
             <meta name="viewport" content="initial-scale=1, width=device-width"/>
-            <tittle>Find your Animal</tittle>
         </Head>
+        <div>{children}</div>
     )
 }
 
